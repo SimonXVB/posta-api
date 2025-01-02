ed4747ef0c982fd2eb8ac627de44eec6632c9139
SimonXVB <167611089+SimonXVB@users.noreply.github.com>
2025-01-02 21:58:59 +0100

    added port to .env

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: process.env.PORT, 
     credentials: true
 }));
 app.use(userRouter);
